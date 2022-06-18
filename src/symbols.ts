d947449768531e4fc85f57e0f817ffbf55ef6e54
Volodymyr Iatsyshyn <viatsyshyn@gmail.com>
2022-06-18 21:23:43 +0300

    updated symbols

--- src/symbols.ts
+++ src/symbols.ts
@@ -1,11 +1,15 @@
 export const SqlQueryDriverSymbol = Symbol.for('IQueryDriver<ISqlQuery>');
-export const SqlQueryBuilderSymbol = Symbol.for('IBuildableQueryCompiler<ISqlQuery>');
+export const SqlQueryBuildableQueryCompilerSymbol = Symbol.for('IBuildableQueryCompiler<ISqlQuery>');
+/** @deprecated use SqlQueryBuildableQueryCompilerSymbol */
+export const SqlQueryBuilderSymbol = SqlQueryBuildableQueryCompilerSymbol;
 export const SqlQueryExecutorProviderSymbol = Symbol.for('IQueryExecutorProvider<ISqlQuery>');
 
 export const TableMetadataSymbol = Symbol.for('TableMetadata');
 
 export const Symbols = {
   SqlQueryDriver: SqlQueryDriverSymbol,
+  SqlQueryBuildableQueryCompiler: SqlQueryBuildableQueryCompilerSymbol,
+  /** @deprecated use SqlQueryBuildableQueryCompilerSymbol */
   SqlQueryBuilder: SqlQueryBuilderSymbol,
   SqlQueryExecutorProvider: SqlQueryExecutorProviderSymbol,
 };
